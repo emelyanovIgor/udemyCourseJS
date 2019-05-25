5ba892079d02102bdb2f2832442a50ae1019a0bd
emelyanovIgor <strike2002@ukr.net>
2019-05-25 11:57:55 +0300

    fist commit

---
+++ my_js.js
@@ -0,0 +1,46 @@
+
+
+/* let test = 'Hello Boys!';
+console.log(test); */
+
+/* console.log(experiment);
+var experiment = 1;
+console.log(experiment); */
+
+//let arr = ['apple.jpeg', 'cucumber.bmp', 'watermelon.png' ];
+//console.log(arr[0]);
+/* let answer = confirm('How are you?');
+console.log(answer); */
+
+/* let answerString = prompt("you are already 18", "Yes, I'm 18");
+let answerNumber = +prompt("you are already 18", "Yes, I'm 18") */
+// console.log(answer);
+//console.log(typeof(answerNumber));
+
+let increment = 10,
+    decrement = 10;
+
+/* increment++;
+decrement--; */
+
+/* console.log(increment++);
+//console.log(increment); 
+console.log(decrement--);
+//console.log(decrement); */
+
+/* 
+let variable1 = 0; //присваивание
+let variable2 = 0; //присваивание
+variable1 == variable2; //проверка на равенство
+variable1 === variable2;//строгая проверка по типам данных
+ */
+
+//console.log("2" == 2); //true
+//console.log("2" == 2); //false
+
+let isChecked = false,
+    isClose = true;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(isChecked || !isClose);
